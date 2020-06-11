57e9705730d57f2e1ac8b8c3ea839dd3e1e45270
belopot <belopot@protonmail.com>
2020-06-11 19:59:16 +0300

    build

--- src/Scene.js
+++ src/Scene.js
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import Figure from './Figure'
 
-const perspective = 2000
+const perspective = 500
 
 export default class Scene {
     constructor() {
